b281e502a010e31e945f2f524b4a060f0d58b728
User <chernyshov.viacheslav@gmail.com>
2023-02-04 03:06:57 +0200

    Initial commit

---
+++ src/main/java/com/scannerbot/ScannerBot/ScannerBotApplication.java
@@ -0,0 +1,13 @@
+package com.scannerbot.ScannerBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScannerBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScannerBotApplication.class, args);
+	}
+
+}
